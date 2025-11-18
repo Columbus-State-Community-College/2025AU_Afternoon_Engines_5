28f2a6748017523d40d72cdef5113a1fc1f7d7d9
Emily <eflauto@student.cscc.edu>
2025-11-18 12:07:46 -0500

    Update Flashlight.cs
    Fix potential divide by 0.

--- 2025AU-Afternoon-Engines-5/Assets/Scripts/Flashlight.cs
+++ 2025AU-Afternoon-Engines-5/Assets/Scripts/Flashlight.cs
@@ -68,6 +68,7 @@ public class Flashlight : MonoBehaviour
 
         if (targetHealthComponent is null) return;
 
+        if (hitAngle == 0f) hitAngle = 0.00001f;
         var damage = baseDamage * Mathf.Pow(rangeModifier, distance / (_lightSource.range / 8)) * (angleModifier / hitAngle);
         
         targetHealthComponent.TakeDamage(damage);
