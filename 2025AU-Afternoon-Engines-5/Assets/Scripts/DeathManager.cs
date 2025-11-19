8d44124b48672dfb411acf7cd398984a34113482
Emily <eflauto@student.cscc.edu>
2025-11-18 19:46:44 -0500

    Update DeathManager.cs
    Updated the death manager to add a specific case for the player.

--- 2025AU-Afternoon-Engines-5/Assets/Scripts/DeathManager.cs
+++ 2025AU-Afternoon-Engines-5/Assets/Scripts/DeathManager.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UIElements;
 
 public class DeathManager : MonoBehaviour
 {
@@ -22,13 +23,24 @@ public class DeathManager : MonoBehaviour
     private void Update()
     {
         if (!_healthSystem.isDead || _deathCalled) return;
-
-        _agent.isStopped = true;
+        
         _deathCalled = true;
-        StartCoroutine(DeathCoroutine());
+        
+        if (transform.CompareTag("Player"))
+        {
+            PlayerDeath();
+        }
+        else
+        {
+            if (_agent)
+            {
+                _agent.isStopped = true;
+            }
+            StartCoroutine(EnemyDeathCoroutine());
+        }
     }
 
-    private IEnumerator DeathCoroutine()
+    private IEnumerator EnemyDeathCoroutine()
     {
         var timer = 0f;
         var initialScale = transform.localScale;
@@ -43,5 +55,12 @@ public class DeathManager : MonoBehaviour
         
         transform.localScale = targetScale;
         Destroy(gameObject);
+    }
+
+    private void PlayerDeath()
+    {
+        var resultText = GameObject.Find("UI").transform.Find("ResultsScreen").transform.Find("ResultText").GetComponent<TextMeshProUGUI>();
+        resultText.text = "You Lose!";
+        Time.timeScale = 0f;
     }
 }
