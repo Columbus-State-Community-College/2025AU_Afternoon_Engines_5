67d7d0779d4bacd105df8bd5b8028f631cc86e88
Emily <eflauto@student.cscc.edu>
2025-11-23 12:38:49 -0500

    Update MainManager
    Updated the main manager to store the music and sound effect volume settings as well as the pause state.

--- 2025AU-Afternoon-Engines-5/Assets/Scripts/MainManager.cs
+++ 2025AU-Afternoon-Engines-5/Assets/Scripts/MainManager.cs
@@ -7,6 +7,11 @@ public class MainManager : MonoBehaviour
     // Game settings
     public float sensX = 2f;
     public float sensY = 2f;
+    public int musicVolume = 50;
+    public int sfxVolume = 50;
+    
+    // State information
+    public bool paused = false;
 
     private void Awake()
     {
